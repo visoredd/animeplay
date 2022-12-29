ab4d8c4a8370c7e28acda93523a92d3da2ac611d
visoredd <guptaayush3108@gmail.com>
2022-12-29 14:47:03 +0530

    fixed some minor bugs

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-	return (
-		<div className='flex justify-center items-center flex-col bg-zinc-800 p-4 w-full mt-10'>
-			<div className='text-gray-400'>Watch HD Anime for Free ©2022-2023 AnimixReplay</div>
-			<div className='text-gray-600'>
-				Disclaimer: This site does not store any files on its server. All contents are
-				provided by non-affiliated third parties. https://discord.gg/animixreplay
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex justify-center items-center flex-col bg-zinc-800 p-4 w-full mt-10">
+      <div className="text-gray-400">
+        Watch HD Anime for Free ©2022-2023 AnimePlay
+      </div>
+      <div className="text-gray-600">
+        Disclaimer: This site does not store any files on its server. All
+        contents are provided by non-affiliated third parties.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Main/Streams/index.jsx
+++ src/components/Main/Streams/index.jsx
@@ -18,7 +18,11 @@ const Streams = () => {
     quality: "",
     isM3U8: true,
   });
-  const { data } = useQuery(["stream-episode", id], () => getStreamLink(id), {
+  const {
+    data,
+    isLoading: streamLoading,
+    isError: streamError,
+  } = useQuery(["stream-episode", id], () => getStreamLink(id), {
     refetchOnMount: false,
     refetchOnWindowFocus: false,
   });
@@ -50,14 +54,16 @@ const Streams = () => {
   }, [data]);
 
   useEffect(() => {
-    if (playedSeconds > 5) {
+    if (!streamLoading && playedSeconds > 5) {
       localStorage.setItem(id, playedSeconds);
     }
   }, [playedSeconds]);
 
   return (
     <div>
-      <Loader loading={isLoading && !isError} />
+      <Loader
+        loading={(isLoading || streamLoading) && (!isError || !streamError)}
+      />
       <div className="mt-10 lg:mx-40 md:mx-20 sm:mx-10 mx-1">
         <div className="bg-zinc-800 w-full p-1 rounded flex justify-between">
           <div className="flex gap-1">
@@ -147,11 +153,11 @@ const Streams = () => {
         </div>
         <div className="bg-zinc-800 w-full p-4 rounded text-2xl text-zinc-300">
           <div>
-            <div className="flex gap-2 text-sm">
-              <div>{selectStream.quality}</div>
-              <div className="border-2 border-zinc-500"></div>
+            <div className="flex gap-2 justify-start items-center text-sm">
+              <div>Quality: {selectStream.quality}</div>
+              <div className="border-2 border-zinc-500 h-full"></div>
               <div
-                className="flex gap-1 text-blue-300 cursor-pointer"
+                className="flex items-center gap-1 text-blue-300 cursor-pointer"
                 onClick={() => {
                   setShowStream(true);
                 }}
@@ -201,6 +207,31 @@ const Streams = () => {
               {animeList?.genres?.join(" ,")}
             </span>
           </div>
+          <div>
+            <div className="flex gap-2 justify-start items-center text-sm">
+              <div
+                className="flex items-center gap-1 text-blue-300 cursor-pointer"
+                onClick={() => {
+                  navigate(`/anime/${animeList.id}`);
+                }}
+              >
+                <div>More Info</div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M11.828 2.25c-.916 0-1.699.663-1.85 1.567l-.091.549a.798.798 0 01-.517.608 7.45 7.45 0 00-.478.198.798.798 0 01-.796-.064l-.453-.324a1.875 1.875 0 00-2.416.2l-.243.243a1.875 1.875 0 00-.2 2.416l.324.453a.798.798 0 01.064.796 7.448 7.448 0 00-.198.478.798.798 0 01-.608.517l-.55.092a1.875 1.875 0 00-1.566 1.849v.344c0 .916.663 1.699 1.567 1.85l.549.091c.281.047.508.25.608.517.06.162.127.321.198.478a.798.798 0 01-.064.796l-.324.453a1.875 1.875 0 00.2 2.416l.243.243c.648.648 1.67.733 2.416.2l.453-.324a.798.798 0 01.796-.064c.157.071.316.137.478.198.267.1.47.327.517.608l.092.55c.15.903.932 1.566 1.849 1.566h.344c.916 0 1.699-.663 1.85-1.567l.091-.549a.798.798 0 01.517-.608 7.52 7.52 0 00.478-.198.798.798 0 01.796.064l.453.324a1.875 1.875 0 002.416-.2l.243-.243c.648-.648.733-1.67.2-2.416l-.324-.453a.798.798 0 01-.064-.796c.071-.157.137-.316.198-.478.1-.267.327-.47.608-.517l.55-.091a1.875 1.875 0 001.566-1.85v-.344c0-.916-.663-1.699-1.567-1.85l-.549-.091a.798.798 0 01-.608-.517 7.507 7.507 0 00-.198-.478.798.798 0 01.064-.796l.324-.453a1.875 1.875 0 00-.2-2.416l-.243-.243a1.875 1.875 0 00-2.416-.2l-.453.324a.798.798 0 01-.796.064 7.462 7.462 0 00-.478-.198.798.798 0 01-.517-.608l-.091-.55a1.875 1.875 0 00-1.85-1.566h-.344zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+
           <div className="flex justify-between text-sm text-zinc-400 mt-2">
             <div>Next Ep: {animeList?.nextAiringEpisode?.episode}</div>
             <div>Total Episodes: {animeList?.totalEpisodes}</div>
